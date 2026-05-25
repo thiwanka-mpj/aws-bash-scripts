914e48f99f0a37dd94bc626b1e90cbd233f19982
Ubuntu <ubuntu@ip-172-31-12-201.ec2.internal>
2026-05-25 14:55:35 +0000

    Added some queries and updated the script

--- aws_resource_tracker.sh
+++ aws_resource_tracker.sh
@@ -3,40 +3,43 @@
 #####################################################################################
 # Author : Janith Thiwanka
 # Date: 25th May 2026
-#
 # Version: v1.0
 #
 # This script will report the AWS resources usage (AWS S3, EC2, Lambda, IAM Users)
 #####################################################################################
 
-{
+set -x  # Enable debug mode to see commands as they execute
 
-# List S3 Bukckets
-echo "Print list of S3 buckets: "
+{
+echo "=========================================="
+echo "AWS Resource Report - $(date)"
+echo "=========================================="
+
+# List S3 Buckets
+echo "Print list of S3 buckets:"
 echo
 aws s3 ls
-echo
 
 # List EC2 Instances
-echo "Print list of EC2 Instances: "
+echo
+echo "Print list of EC2 Instances:"
 echo
 aws ec2 describe-instances \
-	--filters Name=tag-key,Values=Name \
-	--query 'Reservations[*].Instances[*].{Instance:InstanceID, AZ:Placement.AvailabilityZone, Name:Tags[?Key=='Name']|[0].Value}' \
-	--output table
-echo
+    --query 'Reservations[*].Instances[*].{Instance:InstanceId, AZ:Placement.AvailabilityZone, State:State.Name}' \
+    --output table
 
 # List AWS Lambda Functions
-echo "Print list of Lambda Functions: "
 echo
-aws lambda list-functions
+echo "Print list of Lambda Functions:"
 echo
+aws lambda list-functions --query 'Functions[*].FunctionName' --output text
 
-# List IAM Users
-echo "Print list of IAM Users: "
-echo
-aws iam list-users
-echo
-
+# List IAM Users (just usernames for cleaner output)
+echo 
+echo "Print list of IAM Users:"
+aws iam list-users --query 'Users[*].UserName' --output text
 
 } >> resourceTracker
+
+
+
